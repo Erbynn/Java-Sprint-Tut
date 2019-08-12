cc116812e21919fc708c805916a76870fd28ea5b
erbynn <john.erbynn@gmail.com>
2019-08-12 18:52:40 +0100

    date formater added

---
+++ src/main/java/dateFormat/MyDateFormat.java
@@ -0,0 +1,32 @@
+package dateFormat;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+
+public class MyDateFormat {
+    public static void main(String[] args) {
+//        Creating Date object
+        Date today = new Date();
+
+//        printing in default format
+        System.out.println("The date is " + today);
+
+//        ...in mm-dd-yy format
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yy");
+        String formattedDate = sdf.format(today);
+        System.out.println("Date : " + formattedDate);
+
+//        ...in day, Month date, year
+        sdf = new SimpleDateFormat("EEE, MMM d, yyyy");
+        formattedDate = sdf.format(today);
+        System.out.println("Date " + formattedDate);
+
+//        ...in time hour:minute:sec
+        sdf = new SimpleDateFormat("hh;mm;ss a");
+        formattedDate = sdf.format(today);
+        System.out.println("Time: " + formattedDate);
+
+    }
+
+}
