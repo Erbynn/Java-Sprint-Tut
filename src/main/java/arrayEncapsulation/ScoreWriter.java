d8bcd2d05d91cd8230f4894b3674308efc7c489c
erbynn <john.erbynn@gmail.com>
2019-08-13 19:48:03 +0100

    score and scoreWriter created

---
+++ src/main/java/arrayEncapsulation/ScoreWriter.java
@@ -0,0 +1,31 @@
+package arrayEncapsulation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Date;
+
+public class ScoreWriter {
+    /**
+    The method main performs the following actions:
+     1. Creates an instance of array
+     2. Creates score object and populate array with them
+     3. Write the scores data into file
+     */
+
+    public static void main(String[] args) {
+        FileWriter myFile = null;
+        BufferedWriter bWriter = null;
+
+        Date today = new Date();
+        Score[] scores = new Score[3];
+
+//        player number 1
+        scores[0] = new Score();
+        scores[0].setFirstName("John");
+        scores[0].setLastName("Erbynn");
+        scores[0].setScore(200);
+        scores[0].setPlayDate(today);
+
+
+    }
+}
