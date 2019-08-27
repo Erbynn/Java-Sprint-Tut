0e0c402b52d8a8772b46a64a6c5d540ce82facb7
erbynn <john.erbynn@gmail.com>
2019-08-27 13:43:44 +0100

    dog class template created

--- src/main/javatest/methodsUseInstanceVariables3/Dog.java
+++ src/main/javatest/methodsUseInstanceVariables3/Dog.java
@@ -1,5 +1,13 @@
 package methodsUseInstanceVariables3;
 //the state affects method
 public class Dog {
+    int size;
+    String name;
+
+    void bark(){
+        if (size > 60) System.out.println(" Whoof! whoof!!");
+        if (size > 14) System.out.println("Ruff! Ruff!!");
+        else System.out.println("Yip Yip!");
+    }
 
 }
