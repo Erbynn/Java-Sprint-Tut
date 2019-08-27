ee944ee770dfed5bc2d68d644c5f9f6a1bd1c4af
erbynn <john.erbynn@gmail.com>
2019-08-27 07:16:53 +0100

    hobbit classes tested

--- src/main/javatest/arrayObj/Hobbit.java
+++ src/main/javatest/arrayObj/Hobbit.java
@@ -1,1 +1,21 @@
+package arrayObj;
 
+public class Hobbit {
+    String name;
+
+    public static void main(String[] args) {
+        Hobbit[] h = new Hobbit[3];
+        int c = 0;
+
+        while (c < h.length){
+            h[c] = new Hobbit();
+            h[c].name = "bilbo";
+
+            if(c == 1) h[c].name = "frodo";
+            if(c == 2) h[c].name = "sam";
+
+            System.out.println(h[c].name);
+            c = c + 1;
+        }
+    }
+}
