8e888b8a2c758f5c206a5cdeea1472bc25a36524
erbynn <john.erbynn@gmail.com>
2019-08-29 10:24:53 +0100

    dogDriveClass upgraded

--- src/main/javatest/ecapsulationTest/DogTestDrive.java
+++ src/main/javatest/ecapsulationTest/DogTestDrive.java
@@ -2,6 +2,18 @@ package ecapsulationTest;
 
 public class DogTestDrive {
     public static void main(String[] args) {
-        
+
+        Dog dogOne = new Dog();
+        dogOne.setSize(70);
+
+        System.out.println("DogOne of size " + dogOne.getSize() + "barks ");
+        dogOne.bark();
+
+
+        Dog dogTwo = new Dog();
+        dogTwo.setSize(8);
+
+        System.out.println("DogTwo with size " + dogTwo.getSize() + "barks ");
+        dogTwo.bark();
     }
 }
