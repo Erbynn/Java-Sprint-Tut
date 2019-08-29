62e2d1e828d29110dc7d5279207dd3bec7deff2b
erbynn <john.erbynn@gmail.com>
2019-08-29 10:54:27 +0100

    dog object array added to dogDriveClass

--- src/main/javatest/ecapsulationTest/DogTestDrive.java
+++ src/main/javatest/ecapsulationTest/DogTestDrive.java
@@ -5,15 +5,36 @@ public class DogTestDrive {
 
         Dog dogOne = new Dog();
         dogOne.setSize(70);
+        int isize = dogOne.getSize();
 
-        System.out.println("DogOne of size " + dogOne.getSize() + "barks ");
+        System.out.println("DogOne of size " + isize + " barks ");
         dogOne.bark();
 
 
         Dog dogRefTwo = new Dog();
         dogRefTwo.setSize(8);
 
-        System.out.println("DogTwo with size " + dogRefTwo.getSize() + "barks ");
+        System.out.println("\nDogTwo with size " + dogRefTwo.getSize() + " barks ");
         dogRefTwo.bark();
+
+
+//        pet array of 2 references
+        Dog[] pets = new Dog[2];
+
+//        creating dog references
+        pets[0] = new Dog();
+        pets[1] = new Dog();
+
+//        a bit change in how it is set and get
+        pets[0].setSize(80);
+        int size1 = pets[0].getSize();
+        pets[1].setSize(50);
+        int size2 = pets[1].getSize();
+
+        System.out.println("Dog size " + size1);
+        pets[0].bark();
+        System.out.println("Dod size " + size2);
+        pets[1].bark();
+
     }
 }
