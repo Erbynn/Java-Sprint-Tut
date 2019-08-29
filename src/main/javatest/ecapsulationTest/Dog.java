e1dc62c38f856b94500886b0e5192f347ad555b6
erbynn <john.erbynn@gmail.com>
2019-08-29 07:30:39 +0100

    encapsulation test -> dog class created

--- src/main/javatest/ecapsulationTest/Dog.java
+++ src/main/javatest/ecapsulationTest/Dog.java
@@ -1,4 +1,26 @@
 package ecapsulationTest;
 
+import javax.sound.midi.Soundbank;
+
 public class Dog {
+//    data and right protected
+    private int size;
+
+//    getter
+    public int getSize(){
+        return size;
+    }
+
+//    setter
+    public void setSize(int theSize){
+        size = theSize;
+    }
+
+    
+    void bark() {
+        if(size > 60) System.out.println("Whoof! whoof!!");
+        if(size > 14) System.out.println("Ruff! ruff!!");
+        else System.out.println("Yip! yip!!");
+    }
+
 }
