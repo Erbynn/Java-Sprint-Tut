e22e9506c804eef79f393b959f2799bfadfb664c
erbynn <john.erbynn@gmail.com>
2019-08-31 11:22:43 +0100

    .gitignore file modified

---
+++ src/main/javatest/ecapsulationTest/Mix4.java
@@ -0,0 +1,13 @@
+package ecapsulationTest;
+
+public class Magnet {
+    int counter = 0;
+
+    public static void main(String[] args) {
+        int count = 0;
+        Magnet[] m4a = new Magnet[20];
+        int x = 0;
+
+        while ()
+    }
+}
