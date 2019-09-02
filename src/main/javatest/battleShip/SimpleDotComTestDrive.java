c3e43b7ebba16206741892e3be6e75c86d5a3447
erbynn <john.erbynn@gmail.com>
2019-09-02 22:56:03 +0100

    build method logic review

--- src/main/javatest/battleShip/SimpleDotComTestDrive.java
+++ src/main/javatest/battleShip/SimpleDotComTestDrive.java
@@ -2,10 +2,10 @@ package battleShip;
 
 public class SimpleDotComTestDrive {
     public static void main(String[] args) {
-        SimpleDotComTestDrive dot = new SimpleDotComTestDrive();
+        SimpleDotCom dot = new SimpleDotCom();
 
         int[] locations = {2,3,4};
-        dot.setLocations(locations);
+        dot.setLocationCells(locations);
 
         String userGuess = "2";
         String result = dot.checkYourself(userGuess);
