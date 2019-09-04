f24440ea9b8be7a229c03cf3e97aa7763128a6ba
erbynn <john.erbynn@gmail.com>
2019-09-04 10:49:58 +0100

    gitignore modified -> back to norm

--- src/main/javatest/battleShip/Undo.java
+++
@@ -1,7 +0,0 @@
-package battleShip;
-
-public class Undo {
-    public static void main(String[] args) {
-        System.out.println(3);
-    }
-}
--- src/main/javatest/battleShip/Undo2.java
+++
@@ -1,8 +0,0 @@
-package battleShip;
-
-public class Undo2 {
-    public static void main(String[] args) {
-        System.out.println();
-        System.out.println("sout 2");
-    }
-}
