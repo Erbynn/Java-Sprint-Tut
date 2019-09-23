a44ffba5906474dba9daa9165c1ecc2a6f84f383
erbynn <john.erbynn@gmail.com>
2019-09-23 07:42:23 +0100

    serializable object plus file io

--- src/main/javatest/serializationAndIOFiles/Box.java
+++ src/main/javatest/serializationAndIOFiles/Box.java
@@ -1,4 +1,37 @@
-package serializationAndIOFiles;
+/*Dabbling with Serialization and files I/O operations*/
 
-public class Box {
+package serializationAndIOFiles;
+import java.io.*;
+
+//making object flatten and saveble...no methods to implement
+public class Box implements Serializable {
+
+//    saves state - the value of these
+    private int width;
+    private int height;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public static void main(String[] args) {
+
+        Box myBox = new Box();
+        myBox.setHeight(30);
+        myBox.setWidth(50);
+
+//        i/o operations
+        try {
+            FileOutputStream fos = new FileOutputStream("foo.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(myBox);
+            oos.close(); // closing at the objectStream at high-level automatically closes fileStream at low-level
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
