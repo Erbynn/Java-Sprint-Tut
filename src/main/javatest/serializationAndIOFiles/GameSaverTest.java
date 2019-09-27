bad40403480c8262dcd020cb2a2427fb3ec1dbfa
erbynn <john.erbynn@gmail.com>
2019-09-27 08:02:26 +0100

    serialized object setup

---
+++ src/main/javatest/serializationAndIOFiles/GameSaverTest.java
@@ -0,0 +1,9 @@
+package serializationAndIOFiles;
+
+/*
+ * Serializable example
+ * Saving and restoring the game characters
+ * */
+
+public class GameSaverTest {
+}
---
+++ src/main/javatest/serializationAndIOFiles/Pond.java
@@ -0,0 +1,12 @@
+package serializationAndIOFiles;
+
+/*
+* Serialization is all or nothing
+* Either the entire object graph is serialized correctly or serialization fails
+* */
+
+import java.io.Serializable;
+
+public class Pond implements Serializable {
+    private Duck duck
+}
