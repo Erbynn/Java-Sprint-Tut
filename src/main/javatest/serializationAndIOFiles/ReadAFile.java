61fbf3666b97bebe63007cbc42b75f600bfce18d
erbynn <john.erbynn@gmail.com>
2019-09-28 17:40:45 +0100

    ReadAFile commented blockly

---
+++ src/main/javatest/serializationAndIOFiles/ReadAFile.java
@@ -0,0 +1,17 @@
+package serializationAndIOFiles;
+
+/*
+* Reading from a text file.
+* Pretty much simple with these steps;
+*
+* 1. use File object to represent the file
+* 2. FileReader to do the actual reading and
+* 3. BufferedReader to make the reading more efficient
+*
+* NB: Reading happens in a while loop and ends when ReadLine() returns null
+* The most common style to reading data or anything that's NOT a serialized object
+* */
+
+public class ReadAFile {
+
+}
